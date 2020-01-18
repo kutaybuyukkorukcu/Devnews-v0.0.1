a2a8354add7c5c6a7eaa7d9d13919f758b773ef4
kutaybuyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-01-18 19:03:32 +0300

    Commit before making db helper functions

--- src/main/java/service/DataService.java
+++ src/main/java/service/DataService.java
@@ -1,9 +1,16 @@
 package service;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import model.Data;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class DataService {
 
@@ -24,5 +31,31 @@ public class DataService {
 //        }finally {
 //            mongoClient.close();
 //        }
+    }
+
+    public ArrayList<Data> getData(MongoDatabase database) {
+        MongoCollection<Data> collection = database.getCollection("data", Data.class);
+
+        ArrayList<Data> list = new ArrayList<Data>();
+
+        try(MongoCursor<Data> cursor = collection.find().iterator()) {
+            while (cursor.hasNext()) {
+                Data data = cursor.next();
+                list.add(data);
+            }
+        }
+
+        return list;
+    }
+
+    public boolean dataExist(Data data, MongoDatabase database) {
+        MongoCollection<Data> collection = database.getCollection("data", Data.class);
+
+//        Bson queryFilter = eq("title", data.getTitle());
+        Document queryFilter =  new Document("title", data.getTitle());
+
+        FindIterable result = collection.find(queryFilter).limit(1);
+
+        return result.first() == null ? false : true;
     }
 }
